1db7f0447ec87531b7aa59b1427e84346fbe668e
shaikmahaboob435 <151920719+shaikmahaboob435@users.noreply.github.com>
2023-11-24 17:25:45 +0530

    Delete aura directory
    Signed-off-by: shaikmahaboob435 <151920719+shaikmahaboob435@users.noreply.github.com>

--- aura/add/addController.js
+++
@@ -1,7 +0,0 @@
-({
-	add : function(component) {
-        var addvalue=component.get("v.num1") * component.get( "v.num2");
-        component.set("v.sum",addvalue);
-		
-	}
-})
--- aura/addm/addm.app
+++
@@ -1,3 +0,0 @@
-<aura:application >
-    <c:add/>
-</aura:application>
